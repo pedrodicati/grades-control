59d8a13e26aa7ef48b48c8422de23aa61a865655
Pedro Dicati <phgedi48@gmail.com>
2022-11-22 22:12:43 -0300

    feat: create class Student

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-# controle de notas
+# control of grades
 
-class Aluno():
-    def __init__(self, ):
-        pass
+class Student():
+    def __init__(self, name: str, grade1: float, grade2: float):
+        self.name = name
+        self.grade1 = grade1
+        self.grade2 = grade2
+
+        self.average = (self.grade1 + self.grade2) / 2.0
+
+        if(self.average >= 6.0):
+            self.approved = 1
+        else:
+            self.approved = 0
+    
+    def __repr__(self) -> str:
+        if self.approved == 0:
+            approved = 'NOT APPROVED'
+        else:
+            approved = 'APPROVED'
+
+        return f"Name is {self.name}, grade for the first two months is {self.grade1:.2f}, " \
+            f"and for the second {self.grade2:.2f}.\nAverage = {self.average:.2f} and student is {approved}\n"
 
 if __name__ == "__main__":
-    pass
+    students = []
+
+    
